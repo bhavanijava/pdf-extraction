2b08e81297f34bd7993fa60c92155b4e26c94c67
bhavanijava <mecsworldb@gmail.com>
2023-08-17 10:25:27 +0530

    first commit

---
+++ src/main/java/com/web/PdfExtraction/PdfExtractionApplication.java
@@ -0,0 +1,27 @@
+package com.web.PdfExtraction;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.testautomationguru.utility.PDFUtil;
+
+@SpringBootApplication
+public class PdfExtractionApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(PdfExtractionApplication.class, args);
+		
+		
+		String folderLocation="D://Extracted Images";
+		String pdfLocation="C:/Users/Bhavani sankar/Downloads/PdfFormExample.pdf";
+		
+		PDFUtil pdfUtil=new PDFUtil();
+		
+		pdfUtil.setImageDestinationPath(folderLocation);
+		pdfUtil.extractImages(pdfLocation);
+		
+	}
+
+}
