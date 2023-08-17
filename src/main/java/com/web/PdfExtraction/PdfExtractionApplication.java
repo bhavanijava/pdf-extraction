ae70fe7c13e9aab739ae60fc90485a2bb6afa117
bhavanijava <mecsworldb@gmail.com>
2023-08-17 10:33:15 +0530

    new change

--- src/main/java/com/web/PdfExtraction/PdfExtractionApplication.java
+++ src/main/java/com/web/PdfExtraction/PdfExtractionApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.testautomationguru.utility.PDFUtil;
-//remote change
 @SpringBootApplication
 public class PdfExtractionApplication {
 
